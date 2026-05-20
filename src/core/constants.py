2127a864dc9444ac1829b8314374e96cc101c673
mokim9994-cloud <mokim9994@gmail.com>
2026-05-21 00:10:56 +0300

    Add constants.py with screen settings and colors

---
+++ src/core/constants.py
@@ -0,0 +1,23 @@
+"""
+Константы игры
+"""
+
+import pygame
+
+# Разрешение окна
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+
+# Название
+GAME_TITLE = "Страх одиночества"
+
+# Цвета (тёмная психологическая палитра)
+BLACK = (10, 10, 10)
+WHITE = (255, 255, 255)
+DARK_BLUE = (26, 26, 46)
+RED_ACCENT = (233, 69, 96)  # алый для хоррора
+
+# Для меню
+TITLE_FONT_SIZE = 72
+BUTTON_FONT_SIZE = 28
